db1e443ed9c23acc8ae7957863e12d101d5a8b25
ANANYA <ananyasinha788@gmail.com>
2026-07-29 12:21:32 +0530

    remove folder

--- lab2/file4.js
+++ lab2/file4.js
@@ -1,8 +1,9 @@
-import{ mkdir } from "fs/promises";
+import{ mkdir,rm } from "fs/promises";
 
 /*await mkdir("upload");
 console.log("folder created");*/
 /*await mkdir("upload/resume");
 console.log("resume created under upload");*/
-await mkdir("images/profile/logos",{ recursive:true});
-console.log("all folders created");
+/*await mkdir("images/profile/logos",{ recursive:true});
+console.log("all folders created");*/
+await rm("upload",{recursive:true})
