f6447709c43b49d503824c6cb4d76a18b5d143d4
ANANYA <ananyasinha788@gmail.com>
2026-09-16 12:15:05 +0530

    post

--- lab3/prg6.js
+++ lab3/prg6.js
@@ -1,22 +1,35 @@
 import http from "http";
 
 const server = http.createServer((req, res) => {
-    if (req.url === "/") {
-      res.statusCode = 200;
-      res.end("sent with 200");
-    } else if (req.url === "/about" && req.method === "GET") {
-      res.statusCode = 200;
-      res.end("req get");
-    } else if (req.url === "/about" && req.method === "POST") {
-      res.statusCode = 200;
-      res.end("req post");
-    } else if (req.url === "/about" && req.method === "DELETE") {
-      res.statusCode = 200;
-      res.end("req delete");
-    } else {
-      res.statusCode = 404;
-      res.end("Page Not Found");
-    }
-
+  if (req.url === "/" && req.method === "GET") {
+    res.statusCode = 200;
+    res.end("Get Request");
+  } else if (req.url === "/" && req.method === "POST") {
+    // console.log("Request:",req);
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk;
+    });
+    req.on("end", () => {
+      const product = JSON.parse(body);
+      console.log("received products:", product);
+      res.statusCode = 201;
+      res.end(JSON.stringify({ msg: "product added", product }));
+    });
+  } else if (req.url === "/" && req.method === "PUT") {
+    const productId = req.url.split('/').pop();
+      res.statusCode=200;
+      res.end("Put Request");
+  }
+    res.statusCode = 200;
+    res.end("Put Request");
+  } else if (req.url === "/" && req.method === "DELETE") {
+    res.statusCode = 200;
+    res.end("Delete Request");
+  } else {
+    res.statusCode = 404;
+    res.end("request not found");
+  }
 });
-server.listen(5000,()=>console.log("prg6 is running"));
+
+server.listen(5000, () => console.log("prg6 is running"));
